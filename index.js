317bcbb934ce07b4071764da21c1566508d7e667
Dominik Piatek <do.piatek@gmail.com>
2015-12-18 17:02:28 +0000

    Clean compiled code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var React, ref$, Obj, List, keys, values;
-React = require('react');
-ref$ = require('prelude-ls'), Obj = ref$.Obj, List = ref$.List;
-keys = Object.keys(React.DOM);
-values = Obj.map(function(it){
+var React = require('react');
+var prelude = require('prelude-ls'),
+var Obj = prelude.Obj;
+var List = prelude.List;
+var keys = Obj.keys(React.DOM);
+var values = Obj.map(function(it) {
   return React.createFactory(it);
 }, keys);
+
 module.exports = Obj.pairsToObj(List.zip(keys, values));
